6163a3312b7566a7dbc2a3681acfac6ecf14a1ef
Krzysztof Piszko <krzysztof@piszkod.pl>
2020-04-18 21:59:49 +0200

    Added current mode display. A little housekeeping

---
+++ Assets/#LD46/Scripts/UI/ModeUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeUpdate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SelectedAction selectedAction;
+
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedAction = GameObject.Find("GameState").GetComponent<SelectedAction>();
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = selectedAction.selectedAction.ToString();
+    }
+}
