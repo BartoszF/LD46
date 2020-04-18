4e3d1b9076d2b5eb21a8a7b4cd45c85fae70dbfa
BartoszF <felis.bartosz@gmail.com>
2020-04-18 15:06:39 +0200

    Added simple kitchen

---
+++ Assets/#LD46/Scripts/Items/AppleItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleItem : BeltItem
+{
+    void Start() {
+        base.Start();
+    }
+
+    void FixedUpdate() {
+        base.FixedUpdate();
+    }
+}
---
+++ Assets/#LD46/Scripts/Machines/KitchenMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenMachine : Machine
+{
+    void Start()
+    {
+        base.Start();
+    }
+
+    void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
