4494b48b700d89c22a1c47aaf3b9d8fea74498a1
Krzysztof Piszko <krzysztof@piszkod.pl>
2020-04-18 18:20:28 +0200

    Fix tile spacing and beginning of price for buildables

---
+++ Assets/#LD46/Scripts/Buildables/BuildableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildableEntity", menuName = "LD46/BuildableEnity", order = 1)]
+public class BuildableEntity : ScriptableObject
+{
+   public int cost;
+
+   public GameObject prefab;
+
+   public BuildingModeEnum buildingMode;
+
+}
--- Assets/#LD46/Scripts/TileMap.cs
+++ Assets/#LD46/Scripts/TileMap.cs
@@ -8,17 +8,20 @@ public class TileMap : MonoBehaviour
     public int width = 20;
     public int height = 20;
 
+    public float tileSpacing = 0.5f;
+
     public GameObject tilePrefab;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = -width/2; i < width/2; i++)
+        for (float i = -width/2; i < width/2; i+=tileSpacing)
         {
-            for (int j = -height/2; j < height/2; j++)
+            for (float j = -height/2; j < height/2; j+=tileSpacing)
             {
-                Instantiate(tilePrefab, new Vector3(i,j, 2), Quaternion.identity);
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i,j, 2), Quaternion.identity);
+                tile.transform.localScale = new Vector3(tileSpacing, tileSpacing, 0);
             }
         }
         
