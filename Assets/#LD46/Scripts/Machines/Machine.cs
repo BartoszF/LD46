6a7dc1c5c1f15554e5683d29d0131665478f04d6
BartoszF <felis.bartosz@gmail.com>
2020-04-18 10:42:38 +0200

    Simple coffee machine

---
+++ Assets/#LD46/Scripts/Machines/Machine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour
+{
+    public BeltItem itemProduced;
+
+    public float secondsToProduce = 2f;
+
+    private Transform _output;
+    private OutputChecker _outputChecker;
+    private float _timer = 0f;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        _output = transform.Find("Output");
+        _outputChecker = _output.GetComponent<OutputChecker>();
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        if(_timer >= secondsToProduce && !_outputChecker.hasItem) {
+            _timer -= secondsToProduce;
+            Instantiate(itemProduced, _output.position, Quaternion.identity);
+        }
+
+        _timer += Time.deltaTime;
+    }
+}
---
+++ Assets/#LD46/Scripts/OutputChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputChecker : MonoBehaviour
+{
+    public bool hasItem = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<BeltItem>() != null)
+        {
+            hasItem = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.GetComponent<BeltItem>() != null)
+        {
+            hasItem = false;
+        }
+    }
+}
