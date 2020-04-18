5eff823791cf493a42d9e839eec1bc53d35f454d
BartoszF <felis.bartosz@gmail.com>
2020-04-18 09:30:22 +0200

    Scene for transport. Test belt sprite

---
+++ Assets/#LD46/Scripts/ConveyorBelt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed = 1f;
+    private Material _material;
+    private float _currentScroll = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentScroll += speed * Time.deltaTime;
+        _material.mainTextureOffset = new Vector2(_currentScroll, 0);
+    }
+}
