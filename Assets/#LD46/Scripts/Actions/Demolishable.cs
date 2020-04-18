1c355298d5f75f2004315ea16b8dab96cb5986b0
Krzysztof Piszko <krzysztof@piszkod.pl>
2020-04-18 15:45:17 +0200

    Demolision

---
+++ Assets/#LD46/Scripts/Actions/Demolishable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demolishable : MonoBehaviour
+{
+    SelectedAction selectedAction;
+
+    void Start() {
+        selectedAction = GameObject.Find("GameState").GetComponent<SelectedAction>();        
+    }
+
+    void OnMouseDown () {
+         if (selectedAction.selectedAction == SelectedActionEnum.Demolition) {
+             Destroy(gameObject);
+         }
+    }
+}
