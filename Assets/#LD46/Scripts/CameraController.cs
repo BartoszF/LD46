bad91ccfd885eb29988cd58a904fce78c6fb1717
vinterdo <pio4426@gmail.com>
2020-04-20 04:47:33 +0200

    Adjust camera controller to new 3d look

--- Assets/#LD46/Scripts/CameraController.cs
+++ Assets/#LD46/Scripts/CameraController.cs
@@ -10,13 +10,13 @@ public class CameraController : MonoBehaviour
 
     private CinemachineVirtualCamera _vcam;
     private CinemachineTransposer _transposer;
-    private float _scrollTarget = 0f;
+    private float _scrollTarget;
     // Start is called before the first frame update
     void Start()
     {
         _vcam = GetComponent<CinemachineVirtualCamera>();
         _transposer = _vcam.GetCinemachineComponent<CinemachineTransposer>();
-        _scrollTarget = _transposer.m_FollowOffset.z;
+        _scrollTarget = 5;
 
     }
 
@@ -28,30 +28,37 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             direction.x = -1;
+            direction.y = -1;
         }
         if (Input.GetKey(KeyCode.D))
         {
             direction.x = 1;
+            direction.y = 1;
         }
         if (Input.GetKey(KeyCode.W))
         {
+            direction.x = -1;
             direction.y = 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            direction.x = 1;
             direction.y = -1;
         }
 
         if (Mathf.Abs(Input.mouseScrollDelta.y) > 0.05f)
         {
-            _scrollTarget += Input.mouseScrollDelta.y * settings.zoomAmount;
+            _scrollTarget -= Input.mouseScrollDelta.y * settings.zoomAmount;
             _scrollTarget = Mathf.Clamp(_scrollTarget, settings.minZoom, settings.maxZoom);
         }
 
-        float newZ = Mathf.MoveTowards(_transposer.m_FollowOffset.z, _scrollTarget, Time.deltaTime * settings.zoomSpeed);
+        float zoomFactor = Mathf.MoveTowards(_vcam.m_Lens.OrthographicSize, _scrollTarget, Time.deltaTime * settings.zoomSpeed);
 
-        _transposer.m_FollowOffset = new Vector3(_transposer.m_FollowOffset.x, _transposer.m_FollowOffset.y, newZ);
+        zoomFactor = Mathf.Clamp(zoomFactor, settings.minZoom, settings.maxZoom);
+        
+        _transposer.m_FollowOffset = new Vector3(_transposer.m_FollowOffset.x, _transposer.m_FollowOffset.y, -30);
 
-        target.position += direction.normalized * settings.force * Time.deltaTime;
+        _vcam.m_Lens.OrthographicSize = zoomFactor;
+        target.position += direction.normalized * (settings.force * Time.deltaTime);
     }
 }
