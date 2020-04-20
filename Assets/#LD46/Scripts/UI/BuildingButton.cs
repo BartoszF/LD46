de5cb0013238b05ed0ac5e2d5515284c51b932fc
BartoszF <felis.bartosz@gmail.com>
2020-04-20 15:39:41 +0200

    Fixing info panel

--- Assets/#LD46/Scripts/UI/BuildingButton.cs
+++ Assets/#LD46/Scripts/UI/BuildingButton.cs
@@ -5,51 +5,57 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 using DG.Tweening;
+using System;
 
 public class BuildingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
-    public RectTransform infoPanel;
+    public RectTransform infoPanelTransform;
+    public InfoPanel infoPanel;
     public float duration = 0.2f;
     public TMP_Text itemName;
     public TMP_Text description;
     public TMP_Text cost;
 
     private BuildableEntity asset;
+    private RectTransform rectTransform;
+    private int _index;
 
-    void Start()
+
+    void Awake()
     {
-        infoPanel.localScale = new Vector3(1, 0, 1);
-        infoPanel.gameObject.SetActive(false);
+        rectTransform = GetComponent<RectTransform>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void SetItem(BuildableEntity asset)
     {
         this.asset = asset;
+    }
+
+    void SetInfoOnPanel()
+    {
         itemName.text = asset.name;
         description.text = asset.description;
         cost.text = asset.cost.ToString();
     }
+
+    internal void SetInfoPanel(RectTransform infoPanel)
+    {
+        this.infoPanelTransform = infoPanel;
+        this.infoPanel = infoPanel.GetComponent<InfoPanel>();
+        itemName = infoPanel.transform.Find("Name").GetComponent<TMP_Text>();
+        description = infoPanel.transform.Find("Description").GetComponent<TMP_Text>();
+        cost = infoPanel.transform.Find("Money").Find("Value").GetComponent<TMP_Text>();
+
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        infoPanel.gameObject.SetActive(true);
-        DOTween.To(() => infoPanel.localScale, x => infoPanel.localScale = x, new Vector3(1f, 1f, 1f), duration);
+        SetInfoOnPanel();
+        infoPanel.Show(rectTransform);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        var tween = DOTween.To(() => infoPanel.localScale, x => infoPanel.localScale = x, new Vector3(1f, 0f, 1f), duration);
-        tween.onComplete += OnEnd;
-    }
-
-    void OnEnd()
-    {
-        infoPanel.gameObject.SetActive(false);
+        infoPanel.Hide();
     }
 }
